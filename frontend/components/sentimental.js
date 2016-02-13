03b4fc322c60f1b110d42e4940d6de28d994b5fe
Sheri Zada <sheri.h.zada@gmail.com>
2016-02-13 15:16:03 -0800

    front end sentimental analysis and get scores

--- frontend/components/sentimental.js
+++ frontend/components/sentimental.js
@@ -2,7 +2,31 @@ var analyze = require('Sentimental').analyze,
     React = require('react');
 
 var Sentimental = React.createClass({
-  console.log("HIII");
+  fetchTweets: function(){
+
+  },
+  sentimentalize: function(hashtag, response){
+    var analysis = response.map(function(tweet){
+      return analyze(tweet);
+    });
+
+    this.frequencize(hashtag, analysis);
+  },
+  getScores: function(hashtag, data){
+    var hashtag_sentiments = {};
+    hashtag_sentiments[hashtag] = [];
+
+    for (var datum in data){
+      hashtag_sentiments[hashtag].push(datum.score);
+    };
+  },
+  render: function(){
+    return (
+      <div>
+
+      </div>
+    );
+  }
 });
 
 module.exports = Sentimental;
